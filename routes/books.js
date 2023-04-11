7757862823f3db8fa430c35a7295dd467e798467
ChelseyReann <chelseyreann@gmail.com>
2023-04-11 14:57:01 -0400

    adds a function to look up based on title of book

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/", controllers.getBooks);
 router.get("/:id", controllers.getBook);
-//router.get("/:title", controllers.getBookTitle);
+router.get("/id/:title", controllers.getBookTitle);
 router.post("/", controllers.createBook);
 router.put("/:id", controllers.updateBook);
 router.delete("/:id", controllers.deleteBook);
