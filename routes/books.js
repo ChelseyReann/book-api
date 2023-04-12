9fb741ee6eb1808703d75ff2d25816074604df59
ChelseyReann <chelseyreann@gmail.com>
2023-04-12 11:21:47 -0400

    updates js files

--- routes/books.js
+++ routes/books.js
@@ -5,9 +5,9 @@ const router = Router();
 
 router.get("/", controllers.getBooks);
 router.get("/:id", controllers.getBook);
-router.get("/id/:title", controllers.getBookTitle);
-router.get("/id/id/:author", controllers.getBookAuthor);
-router.get("/id/id/id/:rank", controllers.getBookRank);
+router.get("/title/:title", controllers.getBookTitle);
+router.get("/author/:author", controllers.getBookAuthor);
+router.get("/rank/:rank", controllers.getBookRank);
 router.post("/", controllers.createBook);
 router.put("/:id", controllers.updateBook);
 router.delete("/:id", controllers.deleteBook);
